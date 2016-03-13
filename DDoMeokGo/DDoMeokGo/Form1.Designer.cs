9e92239a6e0c97badb25fca4e1cf83becf1b7e86
SeonbiGOD <dongwoo.fast93@gmail.com>
2016-03-13 23:30:05 +0900

    alpha version
    adb를 이용해 블루스택 화면을 캡쳐하고 캡쳐된 이미지에서 원하는 이미지를 찾아 해당 이미지 부분에 클릭 명령을 줄 수 있는 정도까지 구현함.
    
    다음 단계는 구현되어있는 이미지서칭 함수들을 이용하여 게임을 진행하고 기보를 저장하는 부분을 추가해야한다.

---
+++ DDoMeokGo/DDoMeokGo/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace DDoMeokGo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(515, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "새로고침";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownWidth = 130;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ItemHeight = 12;
+            this.comboBox1.Location = new System.Drawing.Point(379, 7);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(130, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gulim", 12F);
+            this.label1.Location = new System.Drawing.Point(301, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "대상기기";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 33);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(600, 397);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(592, 371);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "모니터링";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(580, 330);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 342);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "캡쳐";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(592, 371);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "대전 모드";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(169, 371);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(592, 371);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "관전 모드";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 442);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "DDoMeokGo";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabPage3;
+    }
+}
+
---
+++ DDoMeokGo/DDoMeokGo/Form1.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Resources;
+using System.Threading;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+
+namespace DDoMeokGo
+{
+    public partial class Form1 : Form
+    {
+        private class Sys
+        {
+            public static ProcessStartInfo cmdProcessInfo;
+            public static Process cmdProcess;
+            public static Bitmap screen;
+            public static int delay = 400;
+            public static Thread mainThread;
+            public static bool problem;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+            UpdateDeviceList();
+            Cursor = Cursors.Default;
+        }
+
+        // 블루스택 연결(ADB 이용)
+        private void UpdateDeviceList()
+        {
+            comboBox1.SelectedIndex = -1;
+            comboBox1.Items.Clear();
+            button1.Text = "갱신중";
+            button1.Enabled = false;
+
+            try
+            {
+                string ADBFilename = SearchADBFilename();
+
+                if (!string.IsNullOrWhiteSpace(ADBFilename))
+                {
+                    Sys.cmdProcessInfo = new ProcessStartInfo()
+                    {
+                        FileName = ADBFilename,
+                        Arguments = "devices",
+                        CreateNoWindow = true,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true
+                    };
+                    Sys.cmdProcess = new Process() { StartInfo = Sys.cmdProcessInfo };
+                    Sys.cmdProcess.Start();
+                    string result = Sys.cmdProcess.StandardOutput.ReadToEnd();
+                    string[] deviceList = result.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                    for (int i = 1; i < deviceList.Length; i++)
+                    {
+                        if (deviceList[i].Contains("device") && !deviceList[i].Contains("devices"))
+                        {
+                            string deviceSerialNumber = deviceList[i].Remove(deviceList[i].IndexOf("\t"));
+                            comboBox1.Items.Add(deviceSerialNumber);
+                        }
+                    }
+                    Sys.cmdProcess.WaitForExit();
+                    Sys.cmdProcess.Close();
+                }
+                else
+                {
+                    MessageBox.Show(this, "ADB파일을 찾을 수 없습니다.\n블루스택을 실행해주세요.",
+                        "갱신 실패", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(this, exc.ToString(), "예외 발생", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            button1.Text = "새로고침";
+            button1.Enabled = true;
+            comboBox1.SelectedIndex = comboBox1.Items.Count == 0 ? -1 : 0;
+        }
+
+        // Process 리스트를 통해 ADB 추출
+        private string SearchADBFilename()
+        {
+            string ADBFilename;
+
+            List<Process> processes = new List<Process>();
+            processes.AddRange(Process.GetProcessesByName("HD-Frontend"));
+            if (processes.Count > 0)
+            {
+                string ADBName = null;
+                switch (processes[0].ProcessName)
+                {
+                    case "HD-Frontend":
+                        ADBName = "HD-Adb";
+                        break;
+                }
+                ADBFilename = processes[0].Modules[0].FileName;
+                ADBFilename = string.Format("{0}{1}.exe", ADBFilename.Remove(ADBFilename.LastIndexOf("\\") + 1), ADBName);
+                return ADBFilename;
+            }
+            return null;
+        }
+
+        // Command 명령 함수
+        public void Command(string cmd)
+        {
+            if (string.IsNullOrWhiteSpace(comboBox1.Text))
+                return;
+
+            lock (Sys.cmdProcess)
+            {
+                Sys.cmdProcessInfo.Arguments = string.Format("-s {0} {1}", comboBox1.Text, cmd);
+                Sys.cmdProcess.Start();
+                Sys.cmdProcess.WaitForExit();
+                Sys.cmdProcess.Close();
+            }
+        }
+
+        // Command 클릭 명령
+        public void TargetClick(int x, int y)
+        {
+            Command(string.Format("shell input tap {0} {1}", x, y));
+        }
+
+        // 캡쳐함수
+        public Bitmap TargetCapture()
+        {
+            if (string.IsNullOrWhiteSpace(comboBox1.Text))
+                return null;
+
+            Bitmap image = null;
+            lock (Sys.cmdProcess)
+            {
+                string imageFilename = string.Format(@"{0}\Pictures\DDoMeokGo_Capture.png", Environment.GetEnvironmentVariable("HomePath"));
+                Command("shell screencap -p /sdcard/DDoMeokGo_Capture.png");
+                Command(string.Format(@"pull /sdcard/DDoMeokGo_Capture.png {0}", imageFilename));
+                Command("shell rm /sdcard/DDoMeokGo_Capture.png");
+
+                if (!File.Exists(imageFilename))
+                    return null;
+                StreamReader reader = new StreamReader(imageFilename);
+                image = (Bitmap)Image.FromStream(reader.BaseStream);
+                reader.Close();
+                File.Delete(imageFilename);
+            }
+            return image;
+        }
+
+        // 모니터링 - 화면 캡쳐
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(comboBox1.Text))
+            {
+                MessageBox.Show(this, "대상기기를 선택해주세요.", "캡쳐 실패", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+            button2.Enabled = false;
+
+            Bitmap image = TargetCapture();
+
+            if (tabControl1.SelectedTab.Name.Equals("tabPage1"))
+                pictureBox1.Image = image;
+
+            //Directory.CreateDirectory(@"Screenshots\");
+            //image.Save(string.Format(@"Screenshots\{0}.png", DateTime.Now.ToString("yyyyMMdd_HHmmss")));
+
+            button2.Enabled = true;
+        }
+
+        // Log
+        public void Log(string log)
+        {
+            dataGridView1.Rows.Insert(0, 1);
+            dataGridView1.Rows[0].Cells[0].Value = log;
+        }
+
+        // Bitmap 서치 함수
+        public Rectangle BitmapSearch(Bitmap bigBmp, Bitmap smallBmp, double tolerance)
+        {
+            BitmapData smallData = smallBmp.LockBits(new Rectangle(0, 0, smallBmp.Width, smallBmp.Height),
+                ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            BitmapData bigData = bigBmp.LockBits(new Rectangle(0, 0, bigBmp.Width, bigBmp.Height),
+                ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+
+            int smallStride = smallData.Stride;
+            int bigStride = bigData.Stride;
+
+            int bigWidth = bigBmp.Width;
+            int bigHeight = bigBmp.Height - smallBmp.Height + 1;
+            int smallWidth = smallBmp.Width * 3;
+            int smallHeight = smallBmp.Height;
+
+            Rectangle location = Rectangle.Empty;
+            int margin = Convert.ToInt32(255.0 * tolerance);
+
+            unsafe
+            {
+                byte* pSmall = (byte*)(void*)smallData.Scan0;
+                byte* pBig = (byte*)(void*)bigData.Scan0;
+
+                int smallOffset = smallStride - smallBmp.Width * 3;
+                int bigOffset = bigStride - bigBmp.Width * 3;
+
+                bool matchFound = true;
+
+                for (int y = 0; y < bigHeight; y++)
+                {
+                    for (int x = 0; x < bigWidth; x++)
+                    {
+                        byte* pBigBackup = pBig;
+                        byte* pSmallBackup = pSmall;
+
+                        // Look for the small picture.
+                        for (int i = 0; i < smallHeight; i++)
+                        {
+                            int j = 0;
+                            matchFound = true;
+                            for (j = 0; j < smallWidth; j++)
+                            {
+                                // With tolerance: pSmall value should be between margins.
+                                int inf = pBig[0] - margin;
+                                int sup = pBig[0] + margin;
+                                if (sup < pSmall[0] || inf > pSmall[0])
+                                {
+                                    matchFound = false;
+                                    break;
+                                }
+
+                                pBig++;
+                                pSmall++;
+                            }
+
+                            if (!matchFound) break;
+
+                            // We restore the pointers.
+                            pSmall = pSmallBackup;
+                            pBig = pBigBackup;
+
+                            // Next rows of the small and big pictures.
+                            pSmall += smallStride * (1 + i);
+                            pBig += bigStride * (1 + i);
+                        }
+
+                        // If match found, we return.
+                        if (matchFound)
+                        {
+                            location.X = x;
+                            location.Y = y;
+                            location.Width = smallBmp.Width;
+                            location.Height = smallBmp.Height;
+                            break;
+                        }
+
+                        // If no match found, we restore the pointers and continue.
+                        else
+                        {
+                            pBig = pBigBackup;
+                            pSmall = pSmallBackup;
+                            pBig += 3;
+                        }
+                    }
+                    if (matchFound) break;
+                    pBig += bigOffset;
+                }
+            }
+            bigBmp.UnlockBits(bigData);
+            smallBmp.UnlockBits(smallData);
+            return location;
+        }
+
+        // Work 함수들 ( 이미지 탐색 후 해당 이미지 존재하면 클릭 )
+        public bool Work(int mode, string name)
+        {
+            return Work(mode, name, -1, -1, true, 0.1);
+        }
+        public bool Work(int mode, string name, int clickX, int clickY)
+        {
+            return Work(mode, name, clickX, clickY, true, 0.1);
+        }
+        public bool Work(int mode, string name, int clickX, int clickY, bool capture)
+        {
+            return Work(mode, name, clickX, clickY, capture, 0.1);
+        }
+        public bool Work(int mode, string name, int clickX, int clickY, bool capture, double tolerance)
+        {
+            bool matched = false;
+            if (Sys.problem)
+                mode = mode | 1;
+
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            string strBaseName = string.Format("{0}.Properties.Resources", assembly.GetName().Name);
+            ResourceManager rm = new ResourceManager(strBaseName, assembly);
+
+            for (int i = 0; i < 5 * 60 * 1000 / (Sys.delay == 0 ? 1 : Sys.delay); i++)
+            {
+                if (((mode & 1) == 0 && !Sys.problem) || matched)
+                    Thread.Sleep(Sys.delay);
+
+                if (!Sys.problem && capture)
+                    Sys.screen = TargetCapture();
+                Rectangle location = BitmapSearch(Sys.screen, (Bitmap)rm.GetObject(name), tolerance);
+                Log(string.Format("{0} {1}", name, location == Rectangle.Empty ? "찾기 " + i : "발견"));
+
+                if (location != Rectangle.Empty)
+                {
+                    if (tabControl1.SelectedTab.Name.Equals("tabPage1"))
+                    {
+                        Graphics g = Graphics.FromImage(Sys.screen);
+                        g.DrawRectangle(new Pen(Color.Red, 5), location);
+                        pictureBox1.Image = new Bitmap(Sys.screen.Width, Sys.screen.Height, g);
+                    }
+                    Sys.problem = false;
+                    if (clickX >= 0 && clickY >= 0 && (!matched || (i % 20 == 0)))
+                        TargetClick((clickX == 0) ? location.X + location.Width : clickX,
+                            (clickY == 0) ? location.Y + location.Height : clickY);
+                    if ((mode & 2) == 2)
+                        return true;
+                }
+                else
+                {
+                    if (tabControl1.SelectedTab.Name.Equals("tabPage1"))
+                        pictureBox1.Image = Sys.screen;
+                    if (matched)
+                        return true;
+                    if ((mode & 1) == 1)
+                        return false;
+                }
+            }
+            Do("초기화");
+            return false;
+        }
+
+        // Do 함수( 게임 상의 행동 )
+        public void Do(string type)
+        {
+            switch (type)
+            {
+                /*
+                 *  (삭제될수도있는 함수)
+                    ex) 행동력 구매
+                */
+            }
+        }
+
+        // Run
+        public void Run()
+        {
+            //Sys.problem = true;
+            Sys.screen = TargetCapture();
+
+            /*
+                게임 진행 및 기보 저장 부분이 구현되어야함.
+            */
+        }
+    }
+}
