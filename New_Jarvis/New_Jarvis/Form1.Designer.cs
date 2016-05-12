42cb1500085148cd849c50d4764dde6c4ae53e32
SeonbiGOD <dongwoo.fast93@gmail.com>
2016-05-12 13:49:31 +0900

    Add files via upload

---
+++ New_Jarvis/New_Jarvis/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace New_Jarvis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.metroTabPage1 = new MetroFramework.Controls.MetroTabPage();
+            this.fieldView = new System.Windows.Forms.DataGridView();
+            this.debugButton = new MetroFramework.Controls.MetroButton();
+            this.metroTabControl1.SuspendLayout();
+            this.metroTabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fieldView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.Controls.Add(this.metroTabPage1);
+            this.metroTabControl1.Location = new System.Drawing.Point(23, 63);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.SelectedIndex = 0;
+            this.metroTabControl1.Size = new System.Drawing.Size(434, 470);
+            this.metroTabControl1.TabIndex = 0;
+            this.metroTabControl1.UseSelectable = true;
+            // 
+            // metroTabPage1
+            // 
+            this.metroTabPage1.Controls.Add(this.fieldView);
+            this.metroTabPage1.HorizontalScrollbarBarColor = true;
+            this.metroTabPage1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabPage1.HorizontalScrollbarSize = 10;
+            this.metroTabPage1.Location = new System.Drawing.Point(4, 38);
+            this.metroTabPage1.Name = "metroTabPage1";
+            this.metroTabPage1.Size = new System.Drawing.Size(426, 428);
+            this.metroTabPage1.TabIndex = 0;
+            this.metroTabPage1.Text = "MAIN";
+            this.metroTabPage1.VerticalScrollbarBarColor = true;
+            this.metroTabPage1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabPage1.VerticalScrollbarSize = 10;
+            // 
+            // fieldView
+            // 
+            this.fieldView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.fieldView.Location = new System.Drawing.Point(3, 3);
+            this.fieldView.Name = "fieldView";
+            this.fieldView.RowTemplate.Height = 23;
+            this.fieldView.Size = new System.Drawing.Size(419, 418);
+            this.fieldView.TabIndex = 2;
+            this.fieldView.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.Cell_Changed);
+            // 
+            // debugButton
+            // 
+            this.debugButton.Location = new System.Drawing.Point(374, 34);
+            this.debugButton.Name = "debugButton";
+            this.debugButton.Size = new System.Drawing.Size(75, 23);
+            this.debugButton.TabIndex = 1;
+            this.debugButton.Text = "Debug";
+            this.debugButton.UseSelectable = true;
+            this.debugButton.Click += new System.EventHandler(this.debugButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 553);
+            this.Controls.Add(this.debugButton);
+            this.Controls.Add(this.metroTabControl1);
+            this.Name = "Form1";
+            this.Text = "Jarvis for Gomoku";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.metroTabControl1.ResumeLayout(false);
+            this.metroTabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.fieldView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage1;
+        private System.Windows.Forms.DataGridView fieldView;
+        private MetroFramework.Controls.MetroButton debugButton;
+    }
+}
+
---
+++ New_Jarvis/New_Jarvis/Form1.cs
@@ -0,0 +1,86 @@
+﻿using MetroFramework;
+using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace New_Jarvis
+{
+    public partial class Form1 : MetroForm
+    {
+        private char[,] field = new char[15, 15];
+        private bool turn = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // init field
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    field[i, j] = 'x';
+                }
+            }
+
+            // make field view
+            for (int i = 0; i < 15; i++)
+            {
+                fieldView.Columns.Add("", "");
+                fieldView.Columns[i].Width = 25;
+                fieldView.Rows.Add();
+                fieldView.Rows[i].Height = 25;
+            }
+        }
+
+        private void Cell_Changed(object sender, DataGridViewCellEventArgs e)
+        {
+            if (!turn)
+            {
+                field[e.RowIndex, e.ColumnIndex] = 'B';
+                turn = !turn;
+            }
+            else
+            {
+                turn = !turn;
+                field[e.RowIndex, e.ColumnIndex] = 'W';
+            }
+        }
+
+        private void debugButton_Click(object sender, EventArgs e)
+        {
+            int cnt = 0;
+            string result = "";
+
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    if (cnt < 15)
+                    {
+                        result += field[i, j].ToString();
+                        cnt += 1;
+                    }
+                    else
+                    {
+                        result += '\n';
+                        cnt = 0;
+                        continue;
+                    }
+                }
+            }
+
+            MetroMessageBox.Show(this, result, "Current Field", 300);
+        }
+    }
+}
